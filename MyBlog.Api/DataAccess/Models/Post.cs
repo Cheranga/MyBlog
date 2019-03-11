1fd0af91b82b773f86a7e28f9ed0c51ecae259f7
Cheranga Hatangala <cheranga@gmail.com>
2019-03-12 06:44:10 +1100

    Adding skeleton posts repository.

---
+++ MyBlog.Api/DataAccess/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBlog.Api.DataAccess.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ MyBlog.Api/DataAccess/Repositories/PostsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MyBlog.Api.DataAccess.Models;
+
+namespace MyBlog.Api.DataAccess.Repositories
+{
+    public class PostsRepository
+    {
+        private readonly string _connectionString;
+
+        public PostsRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration[""];
+        }
+
+        public Task<List<Post>> GetPostsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
